4be453c361a7f385fb4b054ae2fb7a16fab84a95
Dirceu Belém <dirceu@fourtime.com>
2021-02-26 16:03:30 -0300

    Removendo Duplicidade de Classes

--- app/src/main/java/com/fingermidia/fmgraph/EntryPoint.kt
+++
@@ -1,6 +0,0 @@
-package com.fingermidia.fmgraph
-
-data class EntryPoint(
-    var value: Double,
-    var text: String
-)
--- app/src/main/java/com/fingermidia/fmgraph/Point.kt
+++
@@ -1,8 +0,0 @@
-package com.fingermidia.fmgraph
-
-data class Point(
-    var x: Double,
-    var y: Double,
-    var value: Double,
-    var text: String
-)
