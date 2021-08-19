90a8218c8349ba68b09f667adbd592e054cfb9d4
Dirceu Belém <dirceu@fourtime.com>
2021-08-18 21:10:46 -0300

    Implementando Gráfico NPS

--- app/src/main/java/com/fingermidia/fmgraph/MainActivity.kt
+++ app/src/main/java/com/fingermidia/fmgraph/MainActivity.kt
@@ -7,25 +7,14 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     var values = ArrayList<EntryPoint>()
-    lateinit var chart: GraphLine
+    lateinit var chart: GraphNPS
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        values.add(EntryPoint(0.0, "13"))
-        values.add(EntryPoint(23.0, "14"))
-        values.add(EntryPoint(50.0, "15"))
-        values.add(EntryPoint(125.0, "16"))
-        values.add(EntryPoint(12.0, "17"))
-        values.add(EntryPoint(34.0, "18"))
-        values.add(EntryPoint(190.0, "19"))
-        values.add(EntryPoint(90.0, "20"))
-
-        chart = findViewById(R.id.chart)
-
-        chart.changeData(values)
+        chart = findViewById(R.id.chart2)
+        chart.changeData(50)
 
     }
 
